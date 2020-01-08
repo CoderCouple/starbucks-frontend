673c5501f3f4e4d796e91b8f6ce1cdb2e9b1ed3e
CoderCouple <fofadiyapayal@gmail.com>
2020-01-07 18:31:23 -0800

    minor fix

---
+++ build/precache-manifest.b86b6c725dece144747678885201b62b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e7b52038637519b7c232df28d33a7e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94f0bc25e28dac47112d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "60c7cde4ae89b13c8b91",
+    "url": "/static/js/2.a755b8c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a755b8c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94f0bc25e28dac47112d",
+    "url": "/static/js/main.e3ffa18c.chunk.js"
+  },
+  {
+    "revision": "b32d8d15c240f1df3ecb",
+    "url": "/static/js/runtime-main.383c15b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
